9571749bf95f840a53f2e6de778415634ca272f2
Joseph Karasek <joseph.karasek@gmail.com>
2016-01-15 10:56:33 -0800

    place global variables into one object mySite, add third, fourth, fifth event handler

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
+// object for holding the user input values
+var mySite = {};
+
 $(document).ready(function(){
-  // global variables
-  var userName = "";
-  var userEmail = "";
 
   // event handler form field one submit - user name
   $('#formFieldOneSubmit').click(function(event){
@@ -12,23 +12,70 @@ $(document).ready(function(){
       alert('Please enter a name to continue');
       return;
     } else {
-      userName = userNameInput;
+      mySite.userName = userNameInput;
     };
     // fix problem with submit
     event.preventDefault();
   });
-  // event handler form field two submit - user email
+
+  // event handler form field two submit - userEmail
   $('#formFieldTwoSubmit').click(function(event){
     // variables
     var userEmailInput = $('input#userEmail').val();
-    // check to make sure a name was entered, then assign assign to global userName
+    // check to make sure an email was entered, then assign assign to global userEmail
     if (userEmailInput === "") {
       alert('Please enter an email address');
       return;
     } else {
-      userEmail = userEmailInput;
+      mySite.userEmail = userEmailInput;
     };
     // fix problem with submit
     event.preventDefault();
   });
+
+  // event handler form field three submit - userGroupType
+  $('#formFieldThreeSubmit').click(function(event){
+    // variables
+    var userGroupTypeInput = $('input[name="optionGroupType"]:checked').val();
+    // check to make sure a name was entered, then assign assign to global userName
+    if (userGroupTypeInput === undefined) {
+      alert('Please pick one of the options');
+      return;
+    } else {
+      mySite.userGroupType = userGroupTypeInput;
+    };
+    // fix problem with submit
+    event.preventDefault();
+  });
+
+  // event handler form field four submit - userGroupType
+  $('#formFieldFourSubmit').click(function(event){
+    // variables
+    var userClimatePreferenceInput = $('input[name="optionClimatePreference"]:checked').val();
+    // check to make sure a name was entered, then assign assign to global userName
+    if (userClimatePreferenceInput === undefined) {
+      alert('Please pick one of the options');
+      return;
+    } else {
+      mySite.userClimatePreference = userClimatePreferenceInput;
+    };
+    // fix problem with submit
+    event.preventDefault();
+  });
+
+  // event handler form field four submit - userGroupType
+  $('#formFieldFiveSubmit').click(function(event){
+    // variables
+    var userDispositionInput = $('input[name="optionDisposition"]:checked').val();
+    // check to make sure a name was entered, then assign assign to global userName
+    if (userDispositionInput === undefined) {
+      alert('Please pick one of the options');
+      return;
+    } else {
+      mySite.userDisposition = userDispositionInput;
+    };
+    // fix problem with submit
+    event.preventDefault();
+  });
+
 });
